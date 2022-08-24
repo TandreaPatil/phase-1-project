7f9cc4b8c440f35fdba7d737b1b0a01336c5fb5c
tandrea patil <tandreayoung@gmail.com>
2022-08-24 12:07:31 -0500

    added 3 event listeners

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
 /* Homepagge loaded when logo is clicked. 
 Will display homepage */
 
-function renderHomePage () {
+// Node
+const mainDiv = () => document.getElementById("main");
+const pizzaFlavorListLink = () => document.getElementById("pizza-flavor-list-link");
 
+// Event Listeners
+function attachPizzaFlavorListClickEvent() {
+    pizzaFlavorListLink().addEventListener("click", renderPizzaFlavorListPage);
 }
-document.addEventListener("DOMContentLoaded", () = {
-  renderHomePage();  
+
+// Event Handlers
+function renderHomePage() {
+    resetMainDiv();
+
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+
+
+    h1.innerText = "Pizza Party Planner Homepage"
+    h1.style.marginTop = "0"
+    p.innerText = "Lorem ipsum dolor..."
+
+    mainDiv().appendChild(h1);
+    mainDiv().appendChild(p);
+}
+
+function renderPizzaFlavorListPage() {
+    resetMainDiv();
+
+    const h1 = document.createElement("h1");
+    const ul = document.createElement("ul"); 
+    const li = document.createElement("li");
+e\
+
+    h1.innerText = "Pizza Flavor List Page"
+    h1.style.marginTop = "0"
+
+    li.innerText = "So many different flavors to choose from!"
+
+    ul.appendChild(li);
+
+    mainDiv().appendChild(ul)
+}
+
+// Helpers
+function resetMainDiv() {
+    mainDiv().innerHTML = ""
+}
+
+//DOM CONTENT LOADED
+document.addEventListener("DOMContentLoaded", () => {
+  renderHomePage (); 
+  attachPizzaFlavorListClickEvent();
 })
