9aa22917d1af4801daf7404bf8e593acb26ca140
tandrea patil <tandreayoung@gmail.com>
2022-08-24 12:55:23 -0500

    corrected final click event on random pizza page

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ Will display homepage */
 const mainDiv = () => document.getElementById("main");
 const pizzaFlavorListLink = () => document.getElementById("pizza-flavor-list-link");
 const homePageLink = () => document.getElementById("home-page-link");
+const randomPizzaLink = () => document.getElementById("random-pizza-link");
+
+
 // Event Listeners
 function attachPizzaFlavorListClickEvent() {
     pizzaFlavorListLink().addEventListener("click", renderPizzaFlavorListPage);
@@ -12,8 +15,12 @@ function attachPizzaFlavorListClickEvent() {
 
 function attachHomePageClickEvent() {
     homePageLink().addEventListener("click", renderHomePage);
-
 }
+
+function attachRandomPizzaClickEvenet() {
+    randomPizzaLink().addEventListener("click", renderRandomPizzaPage);
+
+} 
 
 // Event Handlers
 function renderHomePage() {
@@ -47,6 +54,20 @@ function renderPizzaFlavorListPage() {
     mainDiv().appendChild(ul)
 }
 
+function renderRandomPizzaPage() {
+    resetMainDiv();
+
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+
+    h1.innerText = "Random Pizza Page"
+    p.innerText = "Here's a suggestion that I think you'd love!"
+
+    mainDiv().appendChild(h1)
+    mainDiv().appendChild(p)
+}
+
+
 // Helpers
 function resetMainDiv() {
     mainDiv().innerHTML = ""
@@ -56,5 +77,6 @@ function resetMainDiv() {
 document.addEventListener("DOMContentLoaded", () => {
   renderHomePage (); 
   attachPizzaFlavorListClickEvent();
-  attachHomePageClickEvent()
+  attachHomePageClickEvent();
+  attachRandomPizzaClickEvenet();
 })
