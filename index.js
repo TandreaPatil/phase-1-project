d8194c7f86d812d86fe45f995c3d97e226249999
tandrea patil <tandreayoung@gmail.com>
2022-08-24 12:23:19 -0500

    corrected hompage click

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ Will display homepage */
 // Node
 const mainDiv = () => document.getElementById("main");
 const pizzaFlavorListLink = () => document.getElementById("pizza-flavor-list-link");
-
+const homePageLink = () => document.getElementById("home-page-link");
 // Event Listeners
 function attachPizzaFlavorListClickEvent() {
     pizzaFlavorListLink().addEventListener("click", renderPizzaFlavorListPage);
+}
+
+function attachHomePageClickEvent() {
+    homePageLink().addEventListener("click", renderHomePage);
+
 }
 
 // Event Handlers
@@ -19,7 +24,6 @@ function renderHomePage() {
 
 
     h1.innerText = "Pizza Party Planner Homepage"
-    h1.style.marginTop = "0"
     p.innerText = "Lorem ipsum dolor..."
 
     mainDiv().appendChild(h1);
@@ -31,16 +35,15 @@ function renderPizzaFlavorListPage() {
 
     const h1 = document.createElement("h1");
     const ul = document.createElement("ul"); 
-    const li = document.createElement("li");
-e\
+    const li = document.createElement("li")
 
-    h1.innerText = "Pizza Flavor List Page"
-    h1.style.marginTop = "0"
+    h1.innerText = "Pizza Flavor List Page";
 
-    li.innerText = "So many different flavors to choose from!"
+    li.innerText = "So many different flavors to choose from!";
 
     ul.appendChild(li);
 
+    mainDiv().appendChild(h1)
     mainDiv().appendChild(ul)
 }
 
@@ -53,4 +56,5 @@ function resetMainDiv() {
 document.addEventListener("DOMContentLoaded", () => {
   renderHomePage (); 
   attachPizzaFlavorListClickEvent();
+  attachHomePageClickEvent()
 })
