49534d62ec821106e5a38a879e304f93bea7c7e2
tandrea patil <tandreayoung@gmail.com>
2022-08-24 13:30:02 -0500

    changed pizza theme to random recipe

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ Will display homepage */
 
 // Node
 const mainDiv = () => document.getElementById("main");
-const pizzaFlavorListLink = () => document.getElementById("pizza-flavor-list-link");
+const yummyRecipeListLink = () => document.getElementById("yummy-recipe-list-link");
 const homePageLink = () => document.getElementById("home-page-link");
-const randomPizzaLink = () => document.getElementById("random-pizza-link");
+const randomRecipeLink = () => document.getElementById("random-recipe-link");
 
 
 // Event Listeners
-function attachPizzaFlavorListClickEvent() {
-    pizzaFlavorListLink().addEventListener("click", renderPizzaFlavorListPage);
+function attachYummyRecipeListClickEvent() {
+    yummyRecipeListLink().addEventListener("click", renderYummyRecipeListPage);
 }
 
 function attachHomePageClickEvent() {
     homePageLink().addEventListener("click", renderHomePage);
 }
 
-function attachRandomPizzaClickEvenet() {
-    randomPizzaLink().addEventListener("click", renderRandomPizzaPage);
+function attachRandomRecipeClickEvent() {
+    randomRecipeLink().addEventListener("click", renderRandomRecipePage);
 
 } 
 
@@ -30,21 +30,21 @@ function renderHomePage() {
     const p = document.createElement("p");
 
 
-    h1.innerText = "Pizza Party Planner Homepage"
+    h1.innerText = "Dinner Party Planner Homepage"
     p.innerText = "Lorem ipsum dolor..."
 
     mainDiv().appendChild(h1);
     mainDiv().appendChild(p);
 }
 
-function renderPizzaFlavorListPage() {
+function renderYummyRecipeListPage() {
     resetMainDiv();
 
     const h1 = document.createElement("h1");
     const ul = document.createElement("ul"); 
     const li = document.createElement("li")
 
-    h1.innerText = "Pizza Flavor List Page";
+    h1.innerText = "Yummy Recipe List Page";
 
     li.innerText = "So many different flavors to choose from!";
 
@@ -54,13 +54,13 @@ function renderPizzaFlavorListPage() {
     mainDiv().appendChild(ul)
 }
 
-function renderRandomPizzaPage() {
+function renderRandomRecipePage() {
     resetMainDiv();
 
     const h1 = document.createElement("h1");
     const p = document.createElement("p");
 
-    h1.innerText = "Random Pizza Page"
+    h1.innerText = "Random Recipe Page"
     p.innerText = "Here's a suggestion that I think you'd love!"
 
     mainDiv().appendChild(h1)
@@ -76,7 +76,7 @@ function resetMainDiv() {
 //DOM CONTENT LOADED
 document.addEventListener("DOMContentLoaded", () => {
   renderHomePage (); 
-  attachPizzaFlavorListClickEvent();
+  attachYummyRecipeListClickEvent();
   attachHomePageClickEvent();
-  attachRandomPizzaClickEvenet();
+  attachRandomRecipeClickEvent();
 })
